e28b41b92d5b9c6e8326c080976c1df1be851bbe
halfrice <neel@surrus.net>
2018-06-17 01:35:59 -0700

    log-in menu added with placeholder styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,7 +14,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  loginForm: FormGroup;
+  username: AbstractControl;
+  password: AbstractControl;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.loginForm = fb.group({
+      'username': ['', Validators.required],
+      'password': ['', Validators.required]
+    });
+
+    this.username = this.loginForm.controls['username'];
+    this.password = this.loginForm.controls['password'];
+
+    this.username.valueChanges.subscribe((value: string) => {
+      console.log('Username value changed to: ', value);
+    });
+
+    this.password.valueChanges.subscribe((value: string) => {
+      console.log('Password value changed to: ', value);
+    });
+    this.loginForm.valueChanges.subscribe((form: any) => {
+      console.log('Form changed to: ', form);
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: any): void {
+    console.log('Submitted form with Username: '+form.username+' and Password: '+form.password);
   }
 
 }
